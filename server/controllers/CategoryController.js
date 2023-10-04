bb55f2b8cad21ea25adec913a038c1916f13dcc9
rvnkrwn <rvnkrwn@gmail.com>
2023-10-04 18:18:11 +0700

    update: categories data

--- server/controllers/CategoryController.js
+++ server/controllers/CategoryController.js
@@ -1,11 +1,32 @@
 import {Category} from '../models/Category.js';
 
+export const createCategory = async (req, res) => {
+    try {
+        const data = req.body;
+        const category = await  Category.createCategory(data);
+        return res.status(201).json({message: 'Successfully created category'});
+    } catch (e) {
+        return res.status(400).json({message: 'Failed create category'});
+    }
+}
+
 export const getCategories = async (req, res) => {
     try {
         const categories = await Category.getCategories();
-        console.log(categories)
         if (!categories.length > 0) {
-            return res.status(404).json({message: 'Users is empty'});
+            return res.status(404).json({message: 'Categories is empty'});
+        }
+        return res.status(201).json(categories);
+    } catch (e) {
+        return res.status(500).json({error: e.message});
+    }
+}
+
+export const getCategory = async (req, res) => {
+    try {
+        const categories = await Category.getCategory();
+        if (!categories.length > 0) {
+            return res.status(404).json({message: 'Categories is empty'});
         }
         return res.status(201).json(categories);
     } catch (e) {
--- server/models/Category.js
+++ server/models/Category.js
@@ -1,8 +1,29 @@
 import {dbPool} from '../config/database.js';
 
 export class Category {
+    static createCategory = async (data) => {
+        const [result] = await dbPool.query('INSERT INTO Categories SET ?', data);
+        return result;
+    }
+
     static getCategories = async () => {
         const [result] = await dbPool.query('SELECT * FROM Categories');
         return result;
     }
+
+    static getCategory = async (condition) => {
+        const [result] = await dbPool.query('SELECT * FROM Categories WHERE ?', condition);
+        return result[0];
+    }
+
+    static updateCategory = async (condition, data) => {
+        const [result] = await dbPool.query('UPDATE Categories SET ? WHERE ?', [data, condition]);
+        return result;
+    }
+
+    static deleteCategory = async (condition) => {
+        const [result] = await dbPool.query('DELETE FROM Categories WHERE ?', condition);
+        return result;
+    }
+
 }
