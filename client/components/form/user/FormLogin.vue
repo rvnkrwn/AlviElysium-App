6b60a70c9467cbff2f3377f8fee44e0255145f52
rvnkrwn <rvnkrwn@gmail.com>
2023-10-10 19:45:58 +0700

    fix: paramater

--- client/components/form/user/FormLogin.vue
+++ client/components/form/user/FormLogin.vue
@@ -99,7 +99,7 @@ export default {
           timer: 1500,
         })
         Cookies.set('isLoggedIn', true, { expires: 7 })
-        Cookies.set('token', data.token, { expires: 7, sameSite: 'none', secure: true })
+        Cookies.set('token', data.token, { expires: 7, sameSite: 'Strict', secure: true })
         await this.$store.dispatch('auth/login')
         await this.$router.push('/')
       } catch (e) {
