743f2fadc58cffb5b0413028c4a2b2d4de77db67
Revan Kurniawan <rvnkrwn@gmail.com>
2023-12-07 04:28:09 +0700

    Update nuxt.config.js

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -17,6 +17,13 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
       { name: 'Content-Security-Policy', content: "script-src 'self'" },
     ],
+    script: [
+        {
+          src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1699646698206174',
+          async: true,
+          crossorigin: 'anonymous'
+        }
+      ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
