c9b09755a87383a62779df663ee2173c65730bc1
Revan Kurniawan <rvnkrwn@gmail.com>
2023-12-07 05:30:30 +0700

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,5 @@
-import mysql from "mysql2";
-import dotenv from "dotenv"
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
 
 dotenv.config();
 export const dbPool = mysql.createPool({
